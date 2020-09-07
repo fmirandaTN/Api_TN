1e0c1125a857aa5ccca78ca2883b1a7d6976bd25
fmirandatn <fjmiranda@tenecesito.cl>
2020-09-07 18:32:13 -0300

    adding database env

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -146,6 +146,18 @@ if 'DATABASE_URL' in os.environ:
         'default': dj_database_url.config(default=config('DATABASE_URL'))
     }
 
+elif 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+
 else:
     # LOCAL DATABASE
     # DATABASES = {
